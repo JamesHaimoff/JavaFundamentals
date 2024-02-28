97b5881a0c449539fd0eccf9ff895f3b42be8c48
JamesHaimoff <157529106+JamesHaimoff@users.noreply.github.com>
2024-02-27 19:34:47 -0500

    Add files via upload

---
+++ datetesteractual.java
@@ -0,0 +1,13 @@
+package datetester;
+
+public class datetesteractual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String month = "January"; String weekday = "Sunday"; int dayofmonth = 21; int year = 2024;
+		System.out.println("American Format: " + weekday + ", " + month + " " + dayofmonth + ", " + year);
+		System.out.println("European Format: " + weekday + " " + dayofmonth + " " + month + " " + year);
+
+	}
+
+}
